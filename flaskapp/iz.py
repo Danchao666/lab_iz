7c1bd226b2ad6dcf4b7d6f9c5ad99ec50a2f9e16
Danchao666 <85376458+Danchao666@users.noreply.github.com>
2021-06-07 00:12:11 +0700

    Update iz.py

--- flaskapp/iz.py
+++ flaskapp/iz.py
@@ -20,8 +20,8 @@ SECRET_KEY = 'secret'
 app.config['SECRET_KEY'] = SECRET_KEY
 # используем капчу и полученные секретные ключи с сайта google 
 app.config['RECAPTCHA_USE_SSL'] = False
-app.config['RECAPTCHA_PUBLIC_KEY'] = '6LfjwBYbAAAAAKbFAglM-hzjUlYSlBVThrKZMXI3'
-app.config['RECAPTCHA_PRIVATE_KEY'] = '6LfjwBYbAAAAABTyxuTA5dBsYlLBz9DIt8WvN3IB'
+app.config['RECAPTCHA_PUBLIC_KEY'] = '6LfxtBcbAAAAAJgi6cgsr8wgJmBWpcCoCTv1pJ_e'
+app.config['RECAPTCHA_PRIVATE_KEY'] = '6LfxtBcbAAAAANpv6l0gYsG_CkivIPRUq2_wXV5V'
 app.config['RECAPTCHA_OPTIONS'] = {'theme': 'white'}
 # обязательно добавить для работы со стандартными шаблонами
 from flask_bootstrap import Bootstrap
