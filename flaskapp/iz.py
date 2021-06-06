3231477bc08e41cdd8f04e2aaaa3018038705c76
Danchao666 <85376458+Danchao666@users.noreply.github.com>
2021-06-07 00:26:51 +0700

    Update iz.py

--- flaskapp/iz.py
+++ flaskapp/iz.py
@@ -32,7 +32,7 @@ class NetForm(FlaskForm):
  # валидатор проверяет введение данных после нажатия кнопки submit
  # и указывает пользователю ввести данные если они не введены
  # или неверны
- size = StringField('size', validators = [DataRequired()])
+ flag = request.form.getlist('flag')
  # поле загрузки файла
  # здесь валидатор укажет ввести правильные файлы
  upload = FileField('Load image', validators=[
